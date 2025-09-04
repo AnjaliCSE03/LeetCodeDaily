73dd2d9402b365a5beea42edb2f1d432bc35ad1b
Anjali <148552134+AnjaliCSE03@users.noreply.github.com>
2025-09-04 23:51:40 +0530

    Time: 1 ms (51.16%) | Memory: 40.2 MB (79.08%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> left(n);
-        vector<int> right(n);
         vector<int> result(n);
-
-        left[0] = 1;
-        right[n-1] = 1;
+        result[0] = 1;
+        int right_product = 1;
 
         for(int i = 1; i < n; i++){
-            left[i] = left[i-1]*nums[i-1];
+            result[i] = result[i-1]*nums[i-1];
         }
 
-        for(int i = n-2; i >= 0; i--){
-            right[i] = right[i+1]*nums[i+1];
-        }
-
-        for(int i = 0; i < n; i++){
-            result[i] = left[i]*right[i];
+        for(int i = n-1; i >= 0; i--){
+            result[i] = right_product * result[i];
+            right_product *= nums[i];
         }
 
         return result; 
@@ -70,5 +64,36 @@ public:
 
 //         return result;
 
+//     }
+// };
+
+
+
+
+//APPROACH 3 :- USING LEFT AND RIGHT ARRAYS.
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> left(n);
+//         vector<int> right(n);
+//         vector<int> result(n);
+
+//         left[0] = 1;
+//         right[n-1] = 1;
+
+//         for(int i = 1; i < n; i++){
+//             left[i] = left[i-1]*nums[i-1];
+//         }
+
+//         for(int i = n-2; i >= 0; i--){
+//             right[i] = right[i+1]*nums[i+1];
+//         }
+
+//         for(int i = 0; i < n; i++){
+//             result[i] = left[i]*right[i];
+//         }
+
+//         return result; 
 //     }
 // };
